09bc79f58f8a08545cf7575ecc78e12919b86423
Maryna <kaijou88@gmail.com>
2020-06-08 23:02:02 +0300

    added solution

---
+++ src/main/java/spring/intro/dto/UserResponseDto.java
@@ -0,0 +1,31 @@
+package spring.intro.dto;
+
+public class UserResponseDto {
+    private Long id;
+    private String login;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
